2e9292843511c0e5dc8bda0f74485953ae064f3f
ldco2016 <renaissance.scholar2012@gmail.com>
2017-01-22 22:12:58 -0500

    Testing Express App with Mocha

---
+++ test/app_test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const request = require('supertest');
+cont app = require('../app');
+
+describe('The express app', () => {
+  it('handles a GET request to /api', (done) => {
+    request(app).get('/api').end((err, response) => {
+      console.log(response);
+    });
+  });
+});
